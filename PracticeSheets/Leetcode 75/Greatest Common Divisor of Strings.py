0e8775d6ecb03ef6a9e2e95a8d99e21dd6e4b4ca
Garvit Joshi <garvitjoshi46@gmail.com>
2025-08-27 08:33:24 +0530

    Day2/75

--- PracticeSheets/Leetcode 75/Greatest Common Divisor of Strings.py
+++ PracticeSheets/Leetcode 75/Greatest Common Divisor of Strings.py
@@ -1,2 +1,15 @@
-str1 = "ABCABC"
-str2 = "ABC"
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        n = len(str1)
+        m = len(str2)
+
+        def isDivisor(l):
+            if n% l or m % l:
+                return False
+            f1, f2 = n // l, m // l
+            return str1[:l]*f1 == str1 and str1[:l]*f2 == str2
+
+        for i in range(min(n, m), 0, -1):
+            if isDivisor(i):
+                return str1[:i]
+        return ""
